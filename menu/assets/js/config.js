3b3bc4b0bcb6438ca959738d027b7d4658b8e616
sistematawros-dev <sistematawros@gmail.com>
2025-11-17 00:16:17 -0300

    Update config.js

--- menu/assets/js/config.js
+++ menu/assets/js/config.js
@@ -1,2 +1,7 @@
-// Configuração do cliente
-const API_BASE = localStorage.getItem('API_BASE') || 'http://localhost:3333';
+// config.js
+const DEFAULT_API =
+  location.hostname.endsWith('tawros.com.br')
+    ? 'https://api.sistema.tawros.com.br'
+   // : 'http://localhost:3333';// Configuração do cliente
+//const API_BASE = localStorage.getItem('API_BASE') || 'http://209.38.154.140:3333';
+  const API_BASE = localStorage.getItem('API_BASE') || DEFAULT_API;
